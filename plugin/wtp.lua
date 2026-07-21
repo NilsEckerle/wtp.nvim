4a77f064dae85f98d98526cdad600786d04a3759
Nils Eckerle <nilseckerle2001@protonmail.com>
2026-07-21 15:21:50 +0200

    feat(commands): register Wtp user commands

---
+++ plugin/wtp.lua
@@ -0,0 +1,16 @@
+if vim.g.loaded_wtp then
+	return
+end
+vim.g.loaded_wtp = 1
+
+vim.api.nvim_create_user_command("WtpSwitch", function()
+	require("wtp.pickers").switch()
+end, {})
+
+vim.api.nvim_create_user_command("WtpCreate", function()
+	require("wtp.pickers").create()
+end, {})
+
+vim.api.nvim_create_user_command("WtpDelete", function()
+	require("wtp.pickers").delete()
+end, {})
